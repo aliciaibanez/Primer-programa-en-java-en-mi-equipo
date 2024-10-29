8c48a232037f12a702e50bbdc073e9951dcc4472
Alicia <alicia.ibanez.escudero@gmail.com>
2024-10-29 09:40:13 +0100

    Añadimos tercera línea con la comida favorita

--- Prueba.java
+++ Prueba.java
@@ -2,5 +2,6 @@ public class Prueba {
 	public static void main (String args []){
 		System.out.println ("Ibáñez Escudero");
 		System.out.println ("30, 1994");
+		System.out.println ("Pizza");
 	}
 }
