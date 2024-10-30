422f5483ee9afb13942678c84e24b6f348f40265
Alicia <alicia.ibanez.escudero@gmail.com>
2024-10-30 12:55:13 +0100

    Grupo de música preferido

--- Prueba.java
+++ Prueba.java
@@ -1,7 +1,7 @@
 public class Prueba {
 	public static void main (String args []){
-		System.out.println ("Ibáñez Escudero");
 		System.out.println ("30, 1994");
 		System.out.println ("Pizza");
+		System.out.println ("Linkin Park");
 	}
 }
